bef10552e372aead925dda5840236a0964e9a948
SunnyHuangCodebase <106368104+SunnyHuangCodebase@users.noreply.github.com>
2022-10-05 23:06:40 -0400

    Update Command Pattern
    Add Composite and Undo Commands
    
    Update docstrings.

--- patterns/behavioral/command/browser_commands.py
+++ patterns/behavioral/command/browser_commands.py
@@ -1,11 +1,21 @@
+"""The Browser Command module uses the Command design pattern to send commands.
+
+The Sender never interacts directly with the Receiver, but with a Command Interface.
+A concrete Command then delegates the work to the Receiver.
+
+As a result, the Sender and Receiver are loosely coupled.
+"""
+
 from __future__ import annotations
 from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from enum import Enum
+from typing import Any, Protocol
 
 
 class Key(str, Enum):
   """Keyboard keys."""
+  R = "R"
   T = "T"
   TAB = "Tab"
   W = "W"
@@ -16,90 +26,65 @@ class Modifier(str, Enum):
   """Key modifiers"""
   CTRL = "Ctrl+"
   ALT = "Alt+"
-  SHIFT = "Shift+"
   CTRLSHIFT = "Ctrl+Shift+"
 
 
 @dataclass
-class Hotkey:
-  key: Key
-  command: BrowserCommand
-  modifier: Modifier | str = field(default_factory=str)
-
-  def __str__(self):
-    return f"{self.modifier}{self.key}"
-
-
-@dataclass
-class KeyPress:
+class KeyCombination:
+  """Key combinations."""
   key: Key
   modifier: Modifier | str = field(default_factory=str)
 
-  def __str__(self):
-    return f"{self.modifier}{self.key}"
+  @property
+  def value(self) -> str:
+    return f"{self.modifier}+{self.key}"
 
 
-@dataclass
-class Keyboard:
-  computer: Computer
+class Command(Protocol):
+  """Command that is sent from a Sender to Receiver."""
 
-  def press_key(self, keypress: KeyPress):
-    return self.computer.send_keypress(f"{keypress}")
-
-
-@dataclass
-class Computer:
-  keyboard: Keyboard
-  browser: WebBrowser
-  active_window: App
-
-  def __init__(self, browser: WebBrowser):
-    self.keyboard = Keyboard(self)
-    self.browser = browser
-    self.set_active_window(self.browser)
-
-  def set_active_window(self, app: App):
-    self.active_window = app
-
-  def send_keypress(self, key: str):
-    return self.active_window.receive_keypress(key)
+  def execute(self) -> Any:
+    """Instructs the Receiver to executes a command."""
 
 
-class App(ABC):
-  """A computer application"""
-  hotkeys: dict[str, BrowserCommand]
+class BrowserController:
+  """Controls the browser with commands."""
+  commands: dict[str, BrowserCommand]
 
-  def add_hotkey(self, hotkey: Hotkey):
-    """Link Hotkey to a specific App Command."""
-    self.hotkeys[f"{hotkey}"] = hotkey.command
+  def __init__(self):
+    self.commands = {}
 
-  def receive_keypress(self, key: str):
-    """Receives keyboard input."""
+  def register_hotkey(self, key_combination: KeyCombination,
+                      command: BrowserCommand):
+    """Associates a hotkey to a command."""
+    self.commands[key_combination.value] = command
 
-    if key in self.hotkeys:
-      return self.hotkeys[key].execute()
+  def send_keystrokes(self, key_combination: KeyCombination) -> str | None:
+    """Searches for the keystroke in commands, then executes the command, if any.
+    Otherwise, returns the key pressed."""
+    command = self.commands.get(key_combination.value, None)
 
-    return key
+    if command:
+      return command.execute()
+
+    else:
+      return key_combination.key.value
 
 
-class WebBrowser(App):
+class WebBrowser:
   """An app to view webpages."""
   tabs: list[str]
-  closed_tabs: list[str]
   active_tab_index: int
-  keyboard: Keyboard
 
   def __init__(self, tabs: list[str] | None = None):
     self.tabs = tabs or ["google.com"]
-    self.closed_tabs = []
     self.active_tab_index = 0
-    self.hotkeys = {}
 
   def next_tab_index(self):
     """Changes to the next tab index."""
     self.active_tab_index += 1
 
-    if not self.active_tab_index in range(len(self.tabs)):
+    if self.active_tab_index not in range(len(self.tabs)):
       self.active_tab_index = 0
 
   def prev_tab_index(self):
@@ -113,72 +98,145 @@ class WebBrowser(App):
     """Returns the current tab."""
     return self.tabs[self.active_tab_index]
 
+  def new_tab(self, tab: str = "New Tab"):
+    """Opens a new tab."""
+    self.tabs.append(tab)
+    self.active_tab_index = len(self.tabs) - 1
+
+  def cycle_tab(self, /, *, reverse: bool = False):
+    """Cycles through tab list in order or reverse order."""
+    if not reverse:
+      self.next_tab_index()
+    else:
+      self.prev_tab_index()
+
+  def close_current_tab(self) -> str:
+    """Removes current tab from tabs and adds it to closed tabs."""
+    tab = self.current_tab()
+    self.tabs.remove(tab)
+    self.prev_tab_index()
+    return tab
+
+  def close_all_tabs(self):
+    """Close all tabs and add it to closed tabs."""
+    while self.tabs:
+      self.close_current_tab()
+
 
 class BrowserCommand(ABC):
   """Web Browser commands."""
+  app: WebBrowser
+
+  def __init__(self, app: WebBrowser) -> None:
+    self.app = app
 
   @abstractmethod
-  def execute(self) -> str:
-    """Executes a command."""
+  def execute(self) -> str | None:
+    """Executes a browser command."""
 
 
-@dataclass
 class NewTab(BrowserCommand):
   """Open new browser tab."""
-  browser: WebBrowser
 
-  def execute(self) -> str:
-    self.browser.tabs.append("New Tab")
-    self.browser.next_tab_index()
-    return self.browser.current_tab()
+  def execute(self) -> None:
+    """Opens a new tab."""
+    self.app.new_tab()
 
 
-@dataclass
 class CycleTab(BrowserCommand):
   """Cycles through open browser tabs."""
-  browser: WebBrowser
 
-  def execute(self) -> str:
-    """Goes through the tab list."""
-    self.browser.next_tab_index()
-    return self.browser.current_tab()
+  def execute(self) -> None:
+    """Cycle through the tab list in order."""
+    self.app.cycle_tab()
 
 
-@dataclass
 class ReverseCycleTab(BrowserCommand):
   """Cycles through open browser tabs."""
-  browser: WebBrowser
 
-  def execute(self) -> str:
-    """Goes through the tab list."""
-    self.browser.prev_tab_index()
-    return self.browser.current_tab()
+  def execute(self) -> None:
+    """Cycle through the tab list in reverse order."""
+    self.app.cycle_tab(reverse=True)
+
+
+class CloseAllTabs(BrowserCommand):
+  """Close all open tabs."""
+
+  def execute(self) -> None:
+    """Close tabs."""
+    self.app.close_all_tabs()
 
 
 @dataclass
-class CloseTab(BrowserCommand):
+class CompositeBrowserCommand(BrowserCommand):
+  """A batch of browser commands."""
+  app: WebBrowser
+  commands: list[BrowserCommand] = field(default_factory=list)
+
+  def add(self, command: BrowserCommand) -> None:
+    """Adds a command to the batch of commands."""
+    self.commands.append(command)
+
+  def execute(self) -> None:
+    """Executes all commands."""
+    for command in self.commands:
+      command.execute()
+
+
+class ReversibleTabCommand(BrowserCommand, ABC):
+  """Allows undo browser command."""
+  app: WebBrowser
+  history: TabHistory
+  closed_tab: str
+
+  def __init__(self, app: WebBrowser, history: TabHistory):
+    self.app = app
+    self.history = history
+
+
+class UndoBrowserCommand(BrowserCommand, ABC):
+  """Undo a reversible browser command"""
+  app: WebBrowser
+  history: TabHistory
+  closed_tab: str
+
+  def __init__(self, app: WebBrowser, history: TabHistory):
+    self.app = app
+    self.history = history
+
+
+class CloseTab(ReversibleTabCommand):
   """Closes current browser tab."""
-  browser: WebBrowser
+  app: WebBrowser
+  history: TabHistory
+  closed_tab: str
 
-  def execute(self) -> str:
-    """Removes current tab from tabs and adds it to closed tabs."""
-    tab = self.browser.current_tab()
-    self.browser.tabs.remove(tab)
-    self.browser.prev_tab_index()
-    self.browser.closed_tabs.append(tab)
-    return self.browser.current_tab()
+  def execute(self) -> None:
+    """Closes the current tab."""
+    self.closed_tab = self.app.close_current_tab()
+    self.history.push(self)
+
+
+class UndoCloseTab(UndoBrowserCommand):
+  """Reopen last closed browser tab."""
+
+  def execute(self) -> None:
+    """Opens the last closed tab."""
+    try:
+      last_command = self.history.pop()
+      self.app.new_tab(last_command.closed_tab)
+    except IndexError:
+      pass
 
 
 @dataclass
-class UndoCloseTab(BrowserCommand):
-  """Reopen last closed browser tab."""
-  browser: WebBrowser
+class TabHistory:
+  history: list[ReversibleTabCommand] = field(default_factory=list)
 
-  def execute(self) -> str:
-    """Reopens the last closed tab and activates it."""
-    if not self.browser.closed_tabs:
-      return ""
+  def push(self, command: ReversibleTabCommand):
+    """Pushes a command to the end of history."""
+    self.history.append(command)
 
-    self.browser.active_tab_index = len(self.browser.tabs)
-    self.browser.tabs.append(self.browser.closed_tabs.pop())
-    return self.browser.current_tab()
+  def pop(self):
+    """Pops the last command in history."""
+    return self.history.pop()
--- tests/test_command.py
+++ tests/test_command.py
@@ -1,46 +1,99 @@
 import pytest
 
-from patterns.behavioral.command.browser_commands import CloseTab, Computer, CycleTab, Hotkey, Key, KeyPress, Modifier, NewTab, ReverseCycleTab, UndoCloseTab, WebBrowser
+from patterns.behavioral.command.browser_commands import BrowserController, CloseAllTabs, CloseTab, CompositeBrowserCommand, CycleTab, Key, KeyCombination, Modifier, NewTab, ReverseCycleTab, TabHistory, UndoCloseTab, WebBrowser
 
 
 class TestCommand:
 
   @pytest.fixture
-  def browser(self):
+  def browser(self) -> WebBrowser:
     browser = WebBrowser()
-
-    hotkeys = [
-        Hotkey(Key.T, NewTab(browser), Modifier.CTRL),
-        Hotkey(Key.TAB, CycleTab(browser), Modifier.CTRL),
-        Hotkey(Key.TAB, ReverseCycleTab(browser), Modifier.CTRLSHIFT),
-        Hotkey(Key.W, CloseTab(browser), Modifier.CTRL),
-        Hotkey(Key.F4, CloseTab(browser), Modifier.CTRL),
-        Hotkey(Key.T, UndoCloseTab(browser), Modifier.CTRLSHIFT),
-    ]
-
-    for hotkey in hotkeys:
-      browser.add_hotkey(hotkey)
 
     return browser
 
   @pytest.fixture
-  def pc(self, browser: WebBrowser):
-    return Computer(browser)
+  def controller(self, browser: WebBrowser) -> BrowserController:
+    controller = BrowserController()
 
-  def test_commands(self, pc: Computer):
-    assert pc.keyboard.press_key(KeyPress(Key.T)) == "T"
-    assert pc.keyboard.press_key(KeyPress(Key.T, Modifier.CTRLSHIFT)) == ""
-    assert pc.keyboard.press_key(KeyPress(Key.TAB,
-                                          Modifier.CTRL)) == "google.com"
-    assert pc.keyboard.press_key(KeyPress(Key.TAB,
-                                          Modifier.CTRLSHIFT)) == "google.com"
-    assert pc.keyboard.press_key(KeyPress(Key.T, Modifier.CTRL)) == "New Tab"
-    pc.keyboard.press_key(KeyPress(Key.T, Modifier.CTRL))
-    assert pc.keyboard.press_key(KeyPress(Key.F4, Modifier.CTRL)) == "New Tab"
-    assert pc.keyboard.press_key(KeyPress(Key.W, Modifier.CTRL)) == "google.com"
-    assert pc.keyboard.press_key(KeyPress(Key.T,
-                                          Modifier.CTRLSHIFT)) == "New Tab"
+    reset_browser = CompositeBrowserCommand(browser)
+    reset_browser.add(CloseAllTabs(browser))
+    reset_browser.add(NewTab(browser))
+
+    history = TabHistory()
+    close_tab = CloseTab(browser, history)
+    undo_close_tab = UndoCloseTab(browser, history)
+
+    shortcuts = [
+        (KeyCombination(Key.T, Modifier.CTRL), NewTab(browser)),
+        (KeyCombination(Key.TAB, Modifier.CTRL), CycleTab(browser)),
+        (KeyCombination(Key.TAB, Modifier.CTRLSHIFT), ReverseCycleTab(browser)),
+        (KeyCombination(Key.W, Modifier.CTRL), close_tab),
+        (KeyCombination(Key.F4, Modifier.CTRL), close_tab),
+        (KeyCombination(Key.T, Modifier.CTRLSHIFT), undo_close_tab),
+        (KeyCombination(Key.W, Modifier.CTRLSHIFT), CloseAllTabs(browser)),
+        (KeyCombination(Key.R, Modifier.CTRLSHIFT), reset_browser),
+    ]
+
+    for hotkey, command in shortcuts:
+      controller.register_hotkey(hotkey, command)
+
+    return controller
+
+  def test_new_browser(self, browser: WebBrowser):
+    assert browser.tabs == ["google.com"]
+
+  def test_regular_keystroke(self, browser: WebBrowser,
+                             controller: BrowserController):
+    assert controller.send_keystrokes(KeyCombination(Key.T)) == "T"
+    assert controller.send_keystrokes(KeyCombination(Key.T,
+                                                     Modifier.ALT)) == "T"
+    assert browser.tabs == ["google.com"]
+
+  def test_no_closed_tabs(self, browser: WebBrowser,
+                          controller: BrowserController):
+    controller.send_keystrokes(KeyCombination(Key.T, Modifier.CTRLSHIFT))
+    assert browser.tabs == ["google.com"]
+
+  def test_cycle_through_tabs(self, browser: WebBrowser,
+                              controller: BrowserController):
+
+    controller.send_keystrokes(KeyCombination(Key.TAB, Modifier.CTRL))
+    assert browser.current_tab() == "google.com"
+    controller.send_keystrokes(KeyCombination(Key.TAB, Modifier.CTRL))
+    assert browser.current_tab() == "google.com"
+    controller.send_keystrokes(KeyCombination(Key.TAB, Modifier.CTRLSHIFT))
+    controller.send_keystrokes(KeyCombination(Key.TAB, Modifier.CTRLSHIFT))
+    controller.send_keystrokes(KeyCombination(Key.TAB, Modifier.CTRLSHIFT))
+    assert browser.current_tab() == "google.com"
+
+  def test_new_tab(self, browser: WebBrowser, controller: BrowserController):
+
+    controller.send_keystrokes(KeyCombination(Key.T, Modifier.CTRL))
+    assert browser.current_tab() == "New Tab"
+
+  def test_close_tab(self, browser: WebBrowser, controller: BrowserController):
+    controller.send_keystrokes(KeyCombination(Key.T, Modifier.CTRL))
+    controller.send_keystrokes(KeyCombination(Key.T, Modifier.CTRL))
+    assert browser.current_tab() == "New Tab"
+
+    controller.send_keystrokes(KeyCombination(Key.F4, Modifier.CTRL))
+    assert browser.current_tab() == "New Tab"
+
+    controller.send_keystrokes(KeyCombination(Key.W, Modifier.CTRL))
+    assert browser.current_tab() == "google.com"
+
+    controller.send_keystrokes(KeyCombination(Key.T, Modifier.CTRLSHIFT))
+    assert browser.current_tab() == "New Tab"
+
+  def test_close_all_tabs(self, browser: WebBrowser,
+                          controller: BrowserController):
+    controller.send_keystrokes(KeyCombination(Key.W, Modifier.CTRLSHIFT))
+    assert browser.tabs == []
+
+  def reset_browser(self, browser: WebBrowser, controller: BrowserController):
+    controller.send_keystrokes(KeyCombination(Key.R, Modifier.CTRLSHIFT))
+    assert browser.current_tab() == "New Tab"
 
 
 if __name__ == "__main__":
-  pytest.main([__file__])
+  pytest.main([__file__, "-v"])
